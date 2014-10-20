8037772c0fe3f557c0fe48b45e507ebc509369d1
Alexander Platonov <gorodmehov@gmail.com>
2014-10-20 12:19:11 +0400

    Добавлен новый абстрактный контроллер

---
+++ module/Application/src/Application/Controller/AbstractEntityUsingController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Application\Controller;
+
+use wm\Controller\AbstractActionController;
+
+class AbstractEntityUsingController extends AbstractActionController {
+
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager = null;
+
+    /**
+     * @param EntityManager $em
+     * @access protected
+     * @return PostController
+     */
+    protected function setEntityManager(\Doctrine\ORM\EntityManager $em) {
+        $this->entityManager = $em;
+        return $this;
+    }
+
+    /**
+     * @access protected
+     * @return EntityManager
+     */
+    protected function getEntityManager() {
+        $this->setEntityManager($this->getServiceLocator()->get('Doctrine\ORM\EntityManager'));
+        return $this->entityManager;
+    }
+
+    protected function entitysToArray($entitys) {
+        
+        foreach ($entitys as $entity) {
+            $array[]=$entity->toArray();
+        }
+        return $array;
+    }
+
+}
